8fa55356f42da292e60dda6a90779698bc46a8a4
jay <jay@jays-air.staff.staffs.ac.uk>
2023-10-29 18:13:42 +0000

    ShopFusion Project Setup

---
+++ ShopFusion/src/test/java/com/shopfusion/ShopFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
